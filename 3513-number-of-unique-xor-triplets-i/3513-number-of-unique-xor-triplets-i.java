75b0265e6f034c7e5eab581cb16af6105eefb252
Siddharth Ghawate <143597680+ghawatesiddharth@users.noreply.github.com>
2026-07-23 20:33:21 +0530

    Time: 1 ms (51.02%), Space: 125.4 MB (53.06%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if(n == 1 || n == 2)return n;
+        return 1 << (32 - Integer.numberOfLeadingZeros(n));
+    }
+}
