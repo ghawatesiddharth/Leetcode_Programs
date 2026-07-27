01b9a779d37e4862912a0cc4c18f0c9a5837f795
Siddharth Ghawate <143597680+ghawatesiddharth@users.noreply.github.com>
2026-07-27 12:09:22 +0530

    Time: 3 ms (86.49%), Space: 47.1 MB (97.3%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -2,30 +2,32 @@ class Solution {
     public int maximumProduct(int[] nums) {
         int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE, third = Integer.MIN_VALUE;
         int firstMin = Integer.MAX_VALUE, secondMin = Integer.MAX_VALUE;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > first){
+        
+        for(int i = 0 ;i < nums.length; i++){
+            if(first < nums[i]){
                 third = second;
                 second = first;
-                first = nums[i]; 
+                first = nums[i];
             }
-            else if(nums[i] > second){
+            else if(second < nums[i]){
                 third = second;
                 second = nums[i];
             }
-            else if(nums[i] > third){
+            else if (third < nums[i]){
                 third = nums[i];
             }
-
-            if(nums[i] < firstMin){
+            if(firstMin > nums[i]){
                 secondMin = firstMin;
                 firstMin = nums[i];
             }
-            else if(nums[i] < secondMin){
+            else if(secondMin > nums[i]){
                 secondMin = nums[i];
             }
         }
-        int can1 = first*second*third;
-        int can2 = first*firstMin*secondMin;
-        return Math.max(can1,can2);
+        int ans1 = firstMin * secondMin;
+        ans1*=first;
+        int ans2 = first * second *  third;
+
+        return Math.max(ans1,ans2);
     }
 }
