5f78f18759c61560f8dcf0b3ab7f0b833e2b49c7
Siddharth Ghawate <143597680+ghawatesiddharth@users.noreply.github.com>
2026-08-22 22:29:22 +0530

    Time: 1 ms (83.8%), Space: 42.8 MB (56.08%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,26 +1,27 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        int n = pattern.length();
-        String[] words = s.split(" ");
-        if(n != words.length)return false;
-
+        int n = s.length();
+        int pchar = 0;
         Map<Character,String> map = new HashMap<>();
         Set<String> usedWords = new HashSet<>();
-
-        for(int i = 0;i < n;i++){
-            char pchar = pattern.charAt(i);
-            String word = words[i];
-
-            if(map.containsKey(pchar)){
-                if(!map.get(pchar).equals(word))return false;
-            }            
+        for(int i = 0; i < n; i++){
+            StringBuilder sb = new StringBuilder();
+            while(i < n && s.charAt(i) != ' '){
+                sb.append(s.charAt(i));
+                i++;
+            }
+            String word = sb.toString();
+            if(pchar >= pattern.length()) return false;
+            char ch = pattern.charAt(pchar++);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(word)) return false;
+            }
             else{
-                if(usedWords.contains(word))return false;
-
-                map.put(pchar,word);
+                if(usedWords.contains(word)) return false;
+                map.put(ch, word);
                 usedWords.add(word);
             }
         }
-        return true;
+        return pchar == pattern.length();
     }
 }
