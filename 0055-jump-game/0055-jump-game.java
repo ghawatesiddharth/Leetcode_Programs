7ad28d6567e3a42db425d57ffb13439ab9e1dc85
Siddharth Ghawate <143597680+ghawatesiddharth@users.noreply.github.com>
2026-09-18 11:12:04 +0530

    Time: 66 ms (10.45%), Space: 48.3 MB (8.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        boolean[] dp = new boolean[n];
+        Arrays.fill(dp,false);
+        dp[n-1]=true;
+        for(int i = n-2;i >= 0;i--){
+            int maxreach = Math.min(nums[i]+i,n-1);
+            for(int j = i+1;j <= maxreach;j++){
+                if(dp[j]){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
